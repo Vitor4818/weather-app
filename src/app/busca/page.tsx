e3336b3fe101f023f55add69b14b040022248193
Vitor4818 <vitorg.martins4818@gmail.com>
2024-10-16 05:06:24 -0300

    Implementando funcionalidade

--- src/app/busca/page.tsx
+++ src/app/busca/page.tsx
@@ -1,76 +1,73 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-
-export default function Busca(){
-
-    
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [cityName, setCityName] = useState<string>("")
-    const [cityList, setCityList] = useState([])
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 
-    //Vai pegar o valor do input
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        setCityName (event.target.value)
+export default function Busca() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [cityName, setCityName] = useState<string>("");
+  const [cityList, setCityList] = useState([]); // Tipagem para cityList
+
+  // Vai pegar o valor do input
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCityName(event.target.value);
+  };
+
+  // Vai fazer uma requisição com o valor do input e retornar uma lista de objetos
+  const loadCities = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        `https://brasilapi.com.br/api/cptec/v1/cidade/${cityName}`
+      );
+      const data = await response.json();
+      setCityList(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error); 
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    //vai fazer uma requisição com o valor do input e retornar uma lista de objetos com possiveis respostas, vai guardar na cityName
-    const loadCities = async  () => {
-        setIsLoading(true);
-        
-        try{
-            const response = await fetch (`https://brasilapi.com.br/api/cptec/v1/cidade/${cityName}`)
-            const data = await response.json();
-            setCityList (data)
-            console.log(cityList)
+  
+  const handleClick = () => {
+    loadCities();
+  };
 
-        } catch (error){
-            console.log(error)
-        } finally{
-            setIsLoading(false)
-        }
-        
-        
-    }
-    
-    //Quando clicar no botão vai carregar a função
-    const handleClick = () => {
-        loadCities();
-    };
-
-    const handleNavigate = (cityCode: number) => {
-        const state = {
-          cityCode: cityCode,
-        };
-    
-        navigate("/", { state });
-      };
+  const router = useRouter();
 
 
-    return(
-        <><form>
-            <input
-                label = "Buscar cidade"
-                id="search"
-                name="search"
-                type="text"
-                onChange={handleChange} />
-            <h1>{cityName}</h1>
-            <button type="button" onClick={handleClick}>
-                Buscar
-            </button>
-        </form>
-        
-        <ul>
-            {cityList.map((item)=> (
-                <li>{item.nome}</li>
-            ))}
-        </ul>
+  const handleNavigate = (cityCode: number) => {
+    router.push(`/?cityCode=${cityCode}`);
+  };
 
-         </>
-    )
+  return (
+    <>
+      <form>
+        <input
+          id="search"
+          name="search"
+          type="text"
+          onChange={handleChange}
+          placeholder="Buscar cidade"
+        />
+        <h1>{cityName}</h1>
+        <button type="button" onClick={handleClick} disabled={isLoading}>
+          {isLoading ? "Carregando..." : "Buscar"}
+        </button>
+      </form>
 
+      <ul>
+        {cityList.map((item) => (
+          <li key={item.id} onClick={() => handleNavigate(item.id)}>
+            {item.nome}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
-
